6dde7940f0a50387e93e9ab1d083950a8b881efc
TUKENWONDAY123 <142110422+TUKENWONDAY123@users.noreply.github.com>
2026-04-03 20:44:59 +0300

    Delete env directory

--- env/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL || 'https://afwtmvturyetaqruihsr.supabase.co';
-const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmd3RtdnR1cnlldGFxcnVpaHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMzM3MjUsImV4cCI6MjA5MDcwOTcyNX0.MmBoHJSe5uH_sOhe1WK5egr1znH-4hlm3eBzwgRwL10';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
